a20e40ce8652c93ed3c888f38ab07f53472f570d
Merlin GUYARD <merlin.guyard@gmail.com>
2023-02-16 08:22:06 +0100

    Init

---
+++ pmbv1/src/main/java/com/paymybuddy/pmbv1/Pmbv1Application.java
@@ -0,0 +1,13 @@
+package com.paymybuddy.pmbv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pmbv1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pmbv1Application.class, args);
+	}
+
+}
