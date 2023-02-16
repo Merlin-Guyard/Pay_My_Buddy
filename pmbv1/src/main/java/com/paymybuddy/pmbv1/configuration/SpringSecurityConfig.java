d3a537090b0e6bc120a8dd93a903d5c4fc1fadcc
Merlin GUYARD <merlin.guyard@gmail.com>
2023-02-16 08:37:51 +0100

    Config

---
+++ pmbv1/src/main/java/com/paymybuddy/pmbv1/configuration/SpringSecurityConfig.java
@@ -0,0 +1,47 @@
+package com.mg.paymybuddy.configuration;
+
+import com.mg.paymybuddy.service.CustomUserDetailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    CustomUserDetailService userDetailsService;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+
+    }
+
+    public void configure(HttpSecurity http) throws Exception{
+        http.authorizeRequests()
+                .antMatchers("/css/*", "/jquery/*", "/images/*").permitAll()
+                .antMatchers("/login","/register","/home","/logout").permitAll()
+                .antMatchers("/profile","/contact","/transfer").hasRole("USER")
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login").permitAll()
+                .failureUrl("/login?error=true")
+                .defaultSuccessUrl("/profile", true)
+                .and()
+                .logout()
+                .logoutSuccessUrl("/login");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
---
+++ pmbv1/src/main/java/com/paymybuddy/pmbv1/configuration/SpringWebMvcConfig.java
@@ -0,0 +1,10 @@
+package com.mg.paymybuddy.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+public class SpringWebMvcConfig implements WebMvcConfigurer {
+
+}
