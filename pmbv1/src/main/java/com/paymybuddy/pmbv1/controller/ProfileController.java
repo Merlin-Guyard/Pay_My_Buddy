cc292f3192db1361914d464e18aa018f444cb40b
Merlin GUYARD <merlin.guyard@gmail.com>
2023-03-16 19:39:27 +0100

    added single display

--- pmbv1/src/main/java/com/paymybuddy/pmbv1/controller/ProfileController.java
+++ pmbv1/src/main/java/com/paymybuddy/pmbv1/controller/ProfileController.java
@@ -1,12 +1,16 @@
 package com.paymybuddy.pmbv1.controller;
 
 import com.paymybuddy.pmbv1.model.User;
+import com.paymybuddy.pmbv1.repository.UserRepository;
 import com.paymybuddy.pmbv1.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/profile")
@@ -15,9 +19,18 @@ public class ProfileController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private UserRepository userRepository;
+
     @GetMapping
     public String getProfile(Model model) {
-        model.addAttribute("users", userService.getUsers());
+//        User user = new User("Merlin","Guayrd","Merlin.Guyard@gmail.com","password");
+        Optional<User> oUser = userService.getUserByEmail();
+        User user = oUser.get();
+
+
+
+        model.addAttribute("user", user);
         return "profile";
     }
 
