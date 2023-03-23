a974bba8bdb82f0beb13fa8341ba36b3c112238f
Merlin GUYARD <merlin.guyard@gmail.com>
2023-03-23 22:49:55 +0100

    transfer redirect

--- pmbv1/src/main/java/com/paymybuddy/pmbv1/controller/OperationController.java
+++ pmbv1/src/main/java/com/paymybuddy/pmbv1/controller/OperationController.java
@@ -1,7 +1,6 @@
 package com.paymybuddy.pmbv1.controller;
 
 import com.paymybuddy.pmbv1.model.User;
-import com.paymybuddy.pmbv1.repository.UserRepository;
 import com.paymybuddy.pmbv1.service.ContactService;
 import com.paymybuddy.pmbv1.service.OperationService;
 import com.paymybuddy.pmbv1.service.UserService;
@@ -10,13 +9,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @Controller
 public class OperationController {
@@ -40,11 +36,12 @@ public class OperationController {
     }
 
     @RequestMapping("/operation/transfer")
-    public void transferMoney(Model model,
-                              @NotNull String email,
-                              @NotNull int amount) {
+    public String transferMoney(Model model,
+                                @NotNull String email,
+                                @NotNull int amount) {
 
         System.out.println(operationService.send(email, amount));
+        return "redirect:/operation";
     }
 
 
