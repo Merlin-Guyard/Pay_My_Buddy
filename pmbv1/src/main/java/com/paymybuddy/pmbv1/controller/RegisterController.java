7f04b7634c35254e3add8360b9c8b954c9834d1b
Merlin GUYARD <merlin.guyard@gmail.com>
2023-03-24 01:26:34 +0100

    global error skeleton added

--- pmbv1/src/main/java/com/paymybuddy/pmbv1/controller/RegisterController.java
+++ pmbv1/src/main/java/com/paymybuddy/pmbv1/controller/RegisterController.java
@@ -9,6 +9,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -29,6 +30,10 @@ public class RegisterController {
 
     @PostMapping("/register")
     public String getUser(@Valid @ModelAttribute("user") User user, BindingResult bindingResult, Model model) throws Throwable {
+
+        String err = "Oulala";
+        ObjectError error = new ObjectError("globalError", err);
+        bindingResult.addError(error);
 
         if(bindingResult.hasErrors()){
             return "register";
