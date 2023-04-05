21234dd04714a19cd857c7869902ff1ee2372d88
Merlin GUYARD <merlin.guyard@gmail.com>
2023-04-05 12:45:32 +0200

    started integration test

--- pmbv1/src/test/java/com/paymybuddy/pmbv1/ControllerTest/HomeControllerTest.java
+++ pmbv1/src/test/java/com/paymybuddy/pmbv1/ControllerTest/HomeControllerTest.java
@@ -1,4 +1,40 @@
 package com.paymybuddy.pmbv1.ControllerTest;
 
+
+import com.paymybuddy.pmbv1.controller.HomeController;
+import com.paymybuddy.pmbv1.service.CustomUserDetailService;
+import com.paymybuddy.pmbv1.service.UserService;
+import org.apache.catalina.security.SecurityConfig;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@Import(SecurityConfig.class)
+@WebMvcTest(HomeController.class)
 public class HomeControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private UserService userService;
+
+    @MockBean
+    private CustomUserDetailService customUserDetailService;
+
+    @Test
+    void testReturnHome() throws Exception {
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/home"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.view().name("home"));
+    }
+
 }
