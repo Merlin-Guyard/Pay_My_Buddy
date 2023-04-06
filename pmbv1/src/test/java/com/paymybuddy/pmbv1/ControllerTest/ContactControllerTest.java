3e7d1224295fd73b916ecbd5a23d6466524bfd23
Merlin GUYARD <merlin.guyard@gmail.com>
2023-04-06 18:17:03 +0200

    2nd test halfway
    also updated capital character in testGetContacts

--- pmbv1/src/test/java/com/paymybuddy/pmbv1/ControllerTest/ContactControllerTest.java
+++ pmbv1/src/test/java/com/paymybuddy/pmbv1/ControllerTest/ContactControllerTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
@@ -35,27 +36,47 @@ public class ContactControllerTest {
     private PasswordEncoder passwordEncoder;
 
     @Test
-    @WithMockUser(username = "merlin.guyard@test.com", password = "test123")
+    @WithMockUser(username = "merlin.guyard@test.com", password = "mdp123")
     public void testGetContacts() throws Exception {
 
-        User contact = new User("Tom", "Guyard","tom.guyard@test.com", passwordEncoder.encode("test123"));
+        User contact = new User("Tom", "Guyard", "tom.guyard@test.com", passwordEncoder.encode("mdp123"));
         userRepository.save(contact);
 
-        User user = new User("Merlin", "Guyard", "merlin.guyard@test.com", passwordEncoder.encode("test123"));
+        User user = new User("Merlin", "Guyard", "merlin.guyard@test.com", passwordEncoder.encode("mdp123"));
         List<User> contacts = new ArrayList<>();
         contacts.add(contact);
         user.setFriendList(contacts);
         userRepository.save(user);
 
         mockMvc.perform(MockMvcRequestBuilders.get("/contact")
-                .with(SecurityMockMvcRequestPostProcessors.user("merlin.guyard@test.com").password("test123")))
+                        .with(SecurityMockMvcRequestPostProcessors.user("merlin.guyard@test.com").password("test123")))
                 .andExpect(MockMvcResultMatchers.model().attributeExists("users"))
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(result -> {
                     List<User> users = (List<User>) result.getModelAndView().getModel().get("users");
                     assertThat(users.get(0).getFirstName()).isEqualTo("Tom");
-                    assertThat(users.get(0).getLastName()).isEqualTo("guyard");
+                    assertThat(users.get(0).getLastName()).isEqualTo("Guyard");
                     assertThat(users.get(0).getEmail()).isEqualTo("tom.guyard@test.com");
                 });
+    }
+
+    @Test
+    @WithMockUser(username = "merlin.guyard@test.com", password = "mdp123")
+    public void testAddContacts() throws Exception {
+
+        User contact = new User("Tom", "Guyard", "tom.guyard@test.com", passwordEncoder.encode("mdp123"));
+        userRepository.save(contact);
+        User user = new User("Merlin", "Guyard", "merlin.guyard@test.com", passwordEncoder.encode("mdp123"));
+        userRepository.save(user);
+
+        User contact2Add = new User();
+        contact2Add.setEmail("tom.guyard@test.com");
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/contact/add")
+                        .with(SecurityMockMvcRequestPostProcessors.user("merlin.guyard@test.com").password("test123"))
+                        .param("user", "tom.guyard@test.com")
+                        .flashAttr("user", contact2Add))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.flash().attribute("add_status", "Contact added successfully."));
     }
 }
